b51a9094d67ce100d8eae61860990c7e14bf2f1a
Huongcq98 <86595947+Huongcq98@users.noreply.github.com>
2021-08-26 19:00:04 +0700

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -27,7 +27,7 @@ echo "[!] Tiến hành cài đặt các gói library còn thiếu [!]"
 pkg install -y libpixman libcairo pango xorgproto php libjpeg-turbo-progs libjpeg-turbo-static libjpeg-turbo librsvg librsvg-static nodejs git python wget
 
 git clone https://github.com/Huongcq98/miraiv3
-cd miraiv2
+cd miraiv3
 echo "[!] Đã tải source code thành công, tiến hành cài các gói cần thiết [!]"
 npm install
 echo "[!] Đã cài các gói cần thiết thành công, tiến hành cài đặt file manager [!]"
